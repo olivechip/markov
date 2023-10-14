612dd9e4c9f8b71f44de0ed9232ac10a38b9c871
Oliver <oliver@MSI>
2023-10-14 12:12:53 -0700

    finished markovmachine  cli

--- makeText.js
+++ makeText.js
@@ -1,2 +1,43 @@
 /** Command-line tool to generate Markov text. */
+const fs = require('fs');
+const markov = require('./markov');
+const process = require('process');
+const axios = require('axios');
 
+// make text
+function genText(text){
+    let mm = new markov.MarkovMachine(text);
+    console.log(mm.makeText());
+}
+
+// read file and make text
+function makeText(path){
+    fs.readFile(path, 'utf8', function(err, data){
+        if (err){
+            console.log(`Error reading ${path}: ${err}`);
+            process.exit(1);
+        } else {
+            genText(data);
+        }
+    })
+}
+
+// read url and make text
+async function makeURLText(url){
+    try {
+        const res = await axios.get(url);
+    } catch (err){
+        console.log(`Error reading URL: ${url}, ${err}`);
+        process.exit(1);
+    }
+    genText(res.data);
+}
+
+if (process.argv[2] === 'file'){
+    makeText(process.argv[3]);
+} else if (process.argv[2] === 'url'){
+    makeURLText(process.argv[3]);
+} else {
+    console.log(`Error processing text.`);
+    process.exit(1)
+}
--- markov.js
+++ markov.js
@@ -1,6 +1,5 @@
 /** Textual markov chain generator */
 
-
 class MarkovMachine {
 
   /** build markov machine; read in text.*/
@@ -17,13 +16,50 @@ class MarkovMachine {
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains() {
-    // TODO
+    const map = new Map();
+
+    for (let i = 0; i <= this.words.length-1; i++){
+      let currentWord = this.words[i]
+      let nextWord = this.words[i+1] || null;
+      
+
+      if (map.has(currentWord)){
+        map.get(currentWord).push(nextWord);
+      } else {
+        map.set(currentWord, [nextWord]);
+      }
+    }
+    this.chains = map;
+    // console.log(this.chains);
+    // console.log('end chain')
   }
 
+  // return a random choice from arr
+  static choice(arr){
+    return arr[Math.floor(Math.random()*arr.length)];
+  }
 
   /** return random text from chains */
-
   makeText(numWords = 100) {
-    // TODO
+
+    let keys = Array.from(this.chains.keys());
+    let randomKey = MarkovMachine.choice(keys);
+    let words = [];
+
+    // console.log('arr of map...')
+    // console.log(keys)
+
+    // randomized the first word, pushed into final word array
+    while (words.length < numWords && randomKey !== null){
+      words.push(randomKey);
+      randomKey = MarkovMachine.choice(this.chains.get(randomKey));
+    }
+    // console.log(words)
+    console.log(words.join(" "));
   }
 }
+
+// let mm = new MarkovMachine('i like green eggs and i dislike green ham')
+// mm.makeChains();
+// mm.makeText(5)
+module.exports = { MarkovMachine };
